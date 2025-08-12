c52ec5aef3ec8ef0a3db78ea07bb7c9462e81d66
jacky <jackyfan@163.com>
2025-08-12 18:19:53 +0800

    修改配置

--- OrderManagementSystem/product-service/src/main/java/com/oms/productservice/HomeController.java
+++ OrderManagementSystem/product-service/src/main/java/com/oms/productservice/HomeController.java
@@ -1,13 +1,15 @@
 package com.oms.productservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+    @Value("${oms.greeting}")
+    private String greeting;
     @GetMapping(value = "/")
     public String home(){
-        return "Welcome to the Product Service.";
+        return greeting;
     }
 }
